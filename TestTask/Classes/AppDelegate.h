5eb05e4b49ce7c901353ad731b38e007372c6778
Dmitry Povolotsky <krizai@gmail.com>
2016-09-09 21:35:26 +0200

    Initial commit. Empty project

---
+++ TestTask/Classes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestTask
+//
+//  Created by Dmitry Povolotsky on 9/9/16.
+//  Copyright © 2016 Dmitry Povolotskyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestTask/Classes/DPPhotoSearchViewController.m
@@ -0,0 +1,37 @@
+//
+//  DPPhotoSearchViewController.m
+//  TestTask
+//
+//  Created by Dmitry Povolotsky on 9/9/16.
+//  Copyright © 2016 Dmitry Povolotskyi. All rights reserved.
+//
+
+#import "DPPhotoSearchViewController.h"
+
+@interface DPPhotoSearchViewController ()
+
+@end
+
+@implementation DPPhotoSearchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
